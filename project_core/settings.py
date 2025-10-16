dd352abff1527f0fcef409deac8f0b570b44faab
Kelly Thompson <7luwcuip@students.codeinstitute.net>
2025-10-16 14:39:06 +0100

    feat(templates): add base.html and home.html setup with initial rendering

--- project_core/settings.py
+++ project_core/settings.py
@@ -99,7 +99,10 @@ ROOT_URLCONF = 'project_core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [
+            os.path.join(BASE_DIR / 'templates'),
+            os.path.join(BASE_DIR / 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
