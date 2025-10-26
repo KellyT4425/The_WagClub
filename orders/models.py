44728e1f14a11f815edb3046dabe5c465c966c23
Kelly Thompson <7luwcuip@students.codeinstitute.net>
2025-10-26 17:01:20 +0000

    DB(orders) orders model near completion, requirements updated, linking relations between models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from services.models import Service
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from dateutil.relativedelta import relativedelta
 
 # Create your models here.
+VOUC_STATUS = [
+    ("ISSUED", "ISSUED"),
+    ("REDEEMED", "REDEEMED"),
+    ("EXPIRED", "EXPIRED")
+]
+User = get_user_model()
+
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="orders")
+    item = models.ForeignKey(
+        Service, on_delete=models.SET_NULL, null=True, related_name="service_name")
+
+
+class OrderItem(models.Model):
+    service = models.ForeignKey(
+        Service, on_delete=models.PROTECT, related_name="order_items")
+    orders = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+
+def default_expiry():
+    return timezone.now() + relativedelta(months=18)
+
+
+class Voucher(models.Model):
+    service_order_itm = models.ForeignKey(Service, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="voucher")
+    code = models.CharField(max_length=16, unique=True)
+    qr_img_path = models.ImageField(
+        upload_to='vouchers/qr_codes/', null=True, blank=True)
+    status = models.CharField(choices=VOUC_STATUS, max_length=20)
+    issued_at = models.DateTimeField(auto_now_add=True)
+    redeemed_at = models.DateTimeField(null=True)
+    expires_at = models.DateTimeField(default=default_expiry)
